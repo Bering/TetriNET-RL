1bb8a641d8f90ef59648ee999c8e99a6c5620a06
Bering <bering@ringlogic.com>
2016-02-07 22:00:51 -0500

    Created a half-decent main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.parent.gameObject);
+	}
+
+
+	public void OnExitButtonPushed()
+	{
+		#if UNITY_STANDALONE
+		Application.Quit();
+		#endif
+
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+
+
+	public void OnSoloButtonPushed()
+	{
+		SceneManager.LoadScene ("game");
+	}
+
+
+}
