923cf5df4aa29f4808e5686ce6c5fa198c908b25
Bering <bering@ringlogic.com>
2016-02-08 01:23:39 -0500

    Started the backend
    Pretty late, got to go... Wish this was a jam :-) Not commiting the rest as it's too much WIP

---
+++ Assets/Scripts/PlayArea.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayArea : MonoBehaviour
+{
+	public const int Width = 12;
+	public const int Height = 12;
+
+	static protected int[] grid = new int[Width * Height]; // each int is the index of a block in the blocks's prefabs array
+
+
+	static public int xyToIndex(int x, int y)
+	{
+		return y * Width + x;
+	}
+
+
+	static public bool isPositionEmpty(float x, float y)
+	{
+		return isPositionEmpty (Mathf.FloorToInt (x), Mathf.FloorToInt (y));
+	}
+
+
+	static public bool isPositionEmpty(int x, int y)
+	{
+		return (xyToIndex (x, y) == 0);
+	}
+
+
+}
+
