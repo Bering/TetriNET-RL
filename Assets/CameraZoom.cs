4ace09cbd130db3d66ba7f274b275d7ef5559839
Bering <bering@ringlogic.com>
2016-02-15 08:08:54 -0500

    Added CameraZoom objects
    They make the camera zoom out to make sure that the whole scene is visible, no mater the aspect ratio

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+	SpriteRenderer r;
+
+	void Start ()
+	{
+		r = GetComponent<SpriteRenderer> ();
+	}
+
+
+	void Update()
+	{
+		if(r.isVisible) {
+			Destroy (this.gameObject);
+		}
+
+		Camera.main.orthographicSize += 0.1f;
+	}
+
+}
