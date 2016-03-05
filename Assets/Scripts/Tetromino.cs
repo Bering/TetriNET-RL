0947442cdb0236a7cc919ccd7b85b9d6e28d060b
Bering <bering@ringlogic.com>
2016-03-05 18:24:27 -0500

    Moved functions out of the TetrominoController class into Tetromino class

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -9,6 +9,8 @@ public class Tetromino
 	public int spriteType;
 	public Vector2[] blocks;
 	public PlayArea playArea;
+	public bool hasLanded;
+
 
 	public static Tetromino CreateRandomTetromino()
 	{
@@ -27,8 +29,10 @@ public class Tetromino
 		}
 	}
 
+
 	public void Spawn (PlayArea playArea)
 	{
+		hasLanded = false;
 		this.playArea = playArea;
 		Vector2 spawnPoint = new Vector2 (PlayArea.spawnX, PlayArea.NumberOfRows - 1);
 
@@ -38,5 +42,221 @@ public class Tetromino
 			playArea.SetBlockType (Mathf.FloorToInt(blocks[i].x), Mathf.FloorToInt(blocks[i].y), spriteType);
 		}
 	}
+
+
+	protected void RemoveFromArea()
+	{
+		for (int n = 0; n < blocks.Length; n++) {
+			playArea.SetBlockType (blocks [n], 0);
+		}
+	}
+
+
+	protected void Redraw()
+	{
+		for (int n = 0; n < blocks.Length; n++) {
+			playArea.SetBlockType (blocks [n], spriteType);
+		}
+	}
+
+
+	// This assumes that the block is not on the board. If it is, it will collide with itself.
+	public bool canMoveLeft()
+	{
+		int x, y;
+
+		for (int n = 0; n < blocks.Length; n++) {
+			x = Mathf.RoundToInt (blocks [n].x);
+			y = Mathf.RoundToInt (blocks [n].y);
+
+			if (x == 0) {
+				return false;
+			}
+
+			if (playArea.GetBlockType (x-1, y) != 0)
+				return false;
+		}
+
+		return true;
+	}
+	public void MoveLeft()
+	{
+		RemoveFromArea ();
+
+		if (canMoveLeft ()) {
+			for (int n = 0; n < blocks.Length; n++) {
+				blocks [n].x--;
+			}
+		}
+
+		Redraw ();
+	}
+
+
+	// This assumes that the block is not on the board. If it is, it will collide with itself.
+	public bool canMoveRight()
+	{
+		int x, y;
+
+		for (int n = 0; n < blocks.Length; n++) {
+			x = Mathf.RoundToInt (blocks [n].x);
+			y = Mathf.RoundToInt (blocks [n].y);
+
+			if (x == PlayArea.BlocksPerRow-1) {
+				return false;
+			}
+
+			if (playArea.GetBlockType (x+1, y) != 0)
+				return false;
+		}
+
+		return true;
+	}
+	public void MoveRight()
+	{
+		RemoveFromArea ();
+
+		if (canMoveRight ()) {
+			for (int n = 0; n < blocks.Length; n++) {
+				blocks [n].x++;
+			}
+		}
+
+		Redraw ();
+	}
+
+
+	// This assumes that the block is not on the board. If it is, it will collide with itself.
+	public bool canMoveDown()
+	{
+		int x, y;
+
+		for (int n = 0; n < blocks.Length; n++) {
+			x = Mathf.RoundToInt (blocks [n].x);
+			y = Mathf.RoundToInt (blocks [n].y);
+
+			if (y == 0) {
+				return false;
+			}
+
+			if (playArea.GetBlockType (x, y-1) != 0)
+				return false;
+		}
+
+		return true;
+	}
+	public void MoveDown()
+	{
+		RemoveFromArea ();
+
+		if (canMoveDown ()) {
+			for (int n = 0; n < blocks.Length; n++) {
+				blocks [n].y--;
+			}
+		} else {
+			hasLanded = true;
+		}
+
+		Redraw ();
+	}
+
+
+	public bool canRotateLeft()
+	{
+		Vector2 test;
+
+		for (int n = 0; n < blocks.Length; n++) {
+
+			test = Quaternion.AngleAxis (90, Vector3.forward) * (blocks [n] - blocks [0]);
+			test.x = Mathf.RoundToInt (test.x) + blocks [0].x;
+			test.y = Mathf.RoundToInt (test.y) + blocks [0].y;
+
+
+			if (test.x < 0) {
+				return false;
+			}
+			if (test.x > PlayArea.BlocksPerRow - 1) {
+				return false;
+			}
+
+			if (test.y < 0) {
+				return false;
+			}
+			if (test.y > PlayArea.NumberOfRows - 1) {
+				return false;
+			}
+
+			if (playArea.GetBlockType (Mathf.FloorToInt (test.x), Mathf.FloorToInt (test.y)) != 0) {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+	public void RotateLeft()
+	{
+		RemoveFromArea ();
+
+		if (canRotateLeft ()) {
+			for (int n = 1; n < blocks.Length; n++) {
+				blocks [n] = Quaternion.AngleAxis (90, Vector3.forward) * (blocks [n] - blocks [0]);
+				blocks [n].x = Mathf.RoundToInt (blocks[n].x) + blocks [0].x;
+				blocks [n].y = Mathf.RoundToInt (blocks[n].y) + blocks [0].y;
+			}
+		}
+
+		Redraw ();
+	}
+
+	public bool canRotateRight()
+	{
+		Vector2 test;
+
+		for (int n = 0; n < blocks.Length; n++) {
+
+			test = Quaternion.AngleAxis (-90, Vector3.forward) * (blocks [n] - blocks [0]);
+			test.x = Mathf.RoundToInt (test.x) + blocks [0].x;
+			test.y = Mathf.RoundToInt (test.y) + blocks [0].y;
+
+			// TODO: Refactor these IFs into isBlockValid() and use it in all the can() functions in here
+
+			if (test.x < 0) {
+				return false;
+			}
+			if (test.x > PlayArea.BlocksPerRow - 1) {
+				return false;
+			}
+
+			if (test.y < 0) {
+				return false;
+			}
+			if (test.y > PlayArea.NumberOfRows - 1) {
+				return false;
+			}
+
+			if (playArea.GetBlockType (Mathf.FloorToInt (test.x), Mathf.FloorToInt (test.y)) != 0) {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+	public void RotateRight()
+	{
+		RemoveFromArea ();
+
+		if (canRotateRight ()) {
+			for (int n = 1; n < blocks.Length; n++) {
+				blocks [n] = Quaternion.AngleAxis (-90, Vector3.forward) * (blocks [n] - blocks [0]);
+				blocks [n].x = Mathf.RoundToInt (blocks[n].x) + blocks [0].x;
+				blocks [n].y = Mathf.RoundToInt (blocks[n].y) + blocks [0].y;
+			}
+		}
+
+		Redraw ();
+	}
+
 
 }
--- Assets/Scripts/TetrominoController.cs
+++ Assets/Scripts/TetrominoController.cs
@@ -44,20 +44,19 @@ public class TetrominoController : MonoBehaviour
 	{
 		looping = true;
 		Debug.Log ("Loop started");
+
 		// TODO: Play start sound
 		// TODO: Start soundtrack
+
 		yield return new WaitForSeconds (1f);
 
 		while (looping) {
 			tetromino = SpawnNewTetromino ();
 
-			while (tetromino != null) {
+			while (!tetromino.hasLanded) {
 				yield return new WaitForSeconds (1f);
 
-				// TODO: MoveDown has as a side-effect of nullifying tetromino if it cannot move down. How about calling it TryMoveDown() or something...
-				if (tetromino != null) {
-					MoveDown ();
-				}
+				tetromino.MoveDown ();
 			}
 
 			// TODO: Play block stop sound
@@ -79,21 +78,21 @@ public class TetrominoController : MonoBehaviour
 		}
 
 		if (dropping == true) {
-			MoveDown ();
+			tetromino.MoveDown ();
 		}
 		
 		if (Input.GetAxis("Horizontal") < 0) {
 			lastInputTime = Time.time;
-			MoveLeft ();
+			tetromino.MoveLeft ();
 		}
 		else if (Input.GetAxis("Horizontal") > 0) {
 			lastInputTime = Time.time;
-			MoveRight ();
+			tetromino.MoveRight ();
 		}
 
 		if (Input.GetAxis("Vertical") < 0) {
 			lastInputTime = Time.time;
-			MoveDown ();
+			tetromino.MoveDown ();
 		}
 
 		if (Input.GetButtonDown("Submit")) {
@@ -103,235 +102,14 @@ public class TetrominoController : MonoBehaviour
 
 		if (Input.GetButtonDown("Fire1")) {
 			lastInputTime = Time.time;
-			RotateLeft ();
+			tetromino.RotateLeft ();
 		}
 
 		if (Input.GetButtonDown("Fire2")) {
 			lastInputTime = Time.time;
-			RotateRight ();
+			tetromino.RotateRight ();
 		}
 
 	}
-
-
-	protected void RemoveFromArea()
-	{
-		for (int n = 0; n < tetromino.blocks.Length; n++) {
-			tetromino.playArea.SetBlockType (tetromino.blocks [n], 0);
-		}
-	}
-
-
-	protected void Redraw()
-	{
-		for (int n = 0; n < tetromino.blocks.Length; n++) {
-			tetromino.playArea.SetBlockType (tetromino.blocks [n], tetromino.spriteType);
-		}
-	}
-
-	
-	// This assumes that the block is not on the board. If it is, it will collide with itself.
-	protected bool canMoveLeft()
-	{
-		int x, y;
-
-		for (int n = 0; n < tetromino.blocks.Length; n++) {
-			x = Mathf.RoundToInt (tetromino.blocks [n].x);
-			y = Mathf.RoundToInt (tetromino.blocks [n].y);
-
-			if (x == 0) {
-				return false;
-			}
-
-			if (tetromino.playArea.GetBlockType (x-1, y) != 0)
-				return false;
-		}
-
-		return true;
-	}
-	protected void MoveLeft()
-	{
-		RemoveFromArea ();
-
-		if (canMoveLeft ()) {
-			for (int n = 0; n < tetromino.blocks.Length; n++) {
-				tetromino.blocks [n].x--;
-			}
-		}
-
-		Redraw ();
-	}
-
-
-	// This assumes that the block is not on the board. If it is, it will collide with itself.
-	protected bool canMoveRight()
-	{
-		int x, y;
-
-		for (int n = 0; n < tetromino.blocks.Length; n++) {
-			x = Mathf.RoundToInt (tetromino.blocks [n].x);
-			y = Mathf.RoundToInt (tetromino.blocks [n].y);
-
-			if (x == PlayArea.BlocksPerRow-1) {
-				return false;
-			}
-
-			if (tetromino.playArea.GetBlockType (x+1, y) != 0)
-				return false;
-		}
-
-		return true;
-	}
-	protected void MoveRight()
-	{
-		RemoveFromArea ();
-
-		if (canMoveRight ()) {
-			for (int n = 0; n < tetromino.blocks.Length; n++) {
-				tetromino.blocks [n].x++;
-			}
-		}
-
-		Redraw ();
-	}
-
-
-	// This assumes that the block is not on the board. If it is, it will collide with itself.
-	protected bool canMoveDown()
-	{
-		int x, y;
-
-		for (int n = 0; n < tetromino.blocks.Length; n++) {
-			x = Mathf.RoundToInt (tetromino.blocks [n].x);
-			y = Mathf.RoundToInt (tetromino.blocks [n].y);
-
-			if (y == 0) {
-				return false;
-			}
-
-			if (tetromino.playArea.GetBlockType (x, y-1) != 0)
-				return false;
-		}
-
-		return true;
-	}
-	protected void MoveDown()
-	{
-		RemoveFromArea ();
-
-		if (canMoveDown ()) {
-			for (int n = 0; n < tetromino.blocks.Length; n++) {
-				tetromino.blocks [n].y--;
-			}
-
-			Redraw ();
-		}
-
-		else {
-			Redraw ();
-			tetromino = null;
-		}
-
-	}
-
-
-	protected bool canRotateLeft()
-	{
-		Vector2 test;
-		
-		for (int n = 0; n < tetromino.blocks.Length; n++) {
-
-			test = Quaternion.AngleAxis (90, Vector3.forward) * (tetromino.blocks [n] - tetromino.blocks [0]);
-			test.x = Mathf.RoundToInt (test.x) + tetromino.blocks [0].x;
-			test.y = Mathf.RoundToInt (test.y) + tetromino.blocks [0].y;
-
-
-			if (test.x < 0) {
-				return false;
-			}
-			if (test.x > PlayArea.BlocksPerRow - 1) {
-				return false;
-			}
-
-			if (test.y < 0) {
-				return false;
-			}
-			if (test.y > PlayArea.NumberOfRows - 1) {
-				return false;
-			}
-
-			if (playArea.GetBlockType (Mathf.FloorToInt (test.x), Mathf.FloorToInt (test.y)) != 0) {
-				return false;
-			}
-			
-		}
-
-		return true;
-	}
-	protected void RotateLeft()
-	{
-		RemoveFromArea ();
-
-		if (canRotateLeft ()) {
-			for (int n = 1; n < tetromino.blocks.Length; n++) {
-				tetromino.blocks [n] = Quaternion.AngleAxis (90, Vector3.forward) * (tetromino.blocks [n] - tetromino.blocks [0]);
-				tetromino.blocks [n].x = Mathf.RoundToInt (tetromino.blocks[n].x) + tetromino.blocks [0].x;
-				tetromino.blocks [n].y = Mathf.RoundToInt (tetromino.blocks[n].y) + tetromino.blocks [0].y;
-			}
-		}
-
-		Redraw ();
-	}
-
-	protected bool canRotateRight()
-	{
-		Vector2 test;
-
-		for (int n = 0; n < tetromino.blocks.Length; n++) {
-
-			test = Quaternion.AngleAxis (-90, Vector3.forward) * (tetromino.blocks [n] - tetromino.blocks [0]);
-			test.x = Mathf.RoundToInt (test.x) + tetromino.blocks [0].x;
-			test.y = Mathf.RoundToInt (test.y) + tetromino.blocks [0].y;
-
-
-			// TODO: Refactor these IFs into isBlockValid() and use it in all the can() functions in here
-
-			if (test.x < 0) {
-				return false;
-			}
-			if (test.x > PlayArea.BlocksPerRow - 1) {
-				return false;
-			}
-
-			if (test.y < 0) {
-				return false;
-			}
-			if (test.y > PlayArea.NumberOfRows - 1) {
-				return false;
-			}
-
-			if (playArea.GetBlockType (Mathf.FloorToInt (test.x), Mathf.FloorToInt (test.y)) != 0) {
-				return false;
-			}
-
-		}
-
-		return true;
-	}
-	protected void RotateRight()
-	{
-		RemoveFromArea ();
-
-		if (canRotateRight ()) {
-			for (int n = 1; n < tetromino.blocks.Length; n++) {
-				tetromino.blocks [n] = Quaternion.AngleAxis (-90, Vector3.forward) * (tetromino.blocks [n] - tetromino.blocks [0]);
-				tetromino.blocks [n].x = Mathf.RoundToInt (tetromino.blocks[n].x) + tetromino.blocks [0].x;
-				tetromino.blocks [n].y = Mathf.RoundToInt (tetromino.blocks[n].y) + tetromino.blocks [0].y;
-			}
-		}
-
-		Redraw ();
-	}
-
 
 }
