9a803eada21befcc59ca4f640db9f855f2d394c4
Bering <bering@ringlogic.com>
2016-03-05 23:43:21 -0500

    Made UILabelUpdater, a reusable script that refreshes a UIText

---
+++ Assets/Scripts/UI/UILabelUpdater.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics.SymbolStore;
+
+
+public class UILabelUpdater : MonoBehaviour {
+
+	protected Text label;
+
+	void Awake()
+	{
+		label = GetComponent<Text> ();
+	}
+
+
+	public void UpdateLabelInt(int newValue)
+	{
+		label.text = newValue.ToString();
+	}
+
+	public void UpdateLabelFloat(float newValue)
+	{
+		label.text = newValue.ToString();
+	}
+
+	public void UpdateLabelString(string newValue)
+	{
+		label.text = newValue;
+	}
+
+
+	public void AppendInt(int valueToAdd)
+	{
+		label.text += valueToAdd.ToString();
+	}
+
+	public void AppendFloat(float valueToAdd)
+	{
+		label.text += valueToAdd.ToString();
+	}
+
+	public void AppendString(string valueToAdd)
+	{
+		label.text += valueToAdd;
+	}
+}
