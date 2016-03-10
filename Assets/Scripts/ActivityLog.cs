d881da0d9aacb43def5517a048b24ef21d7e2127
Bering <bering@ringlogic.com>
2016-03-10 08:55:19 -0500

    Added ActivityLog and GameManager services

---
+++ Assets/Scripts/ActivityLog.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using System.Text;
+using System;
+
+public class ActivityLog : MonoBehaviour {
+
+	protected List<string> lines;
+
+	[Serializable] public class ContentChangedEventClass : UnityEvent<string> {}
+	public ContentChangedEventClass ContentChangedEvent;
+
+
+	void Awake () {
+		Services.Register<ActivityLog> (this);
+
+		lines = new List<string>();
+		// TODO: Register as a service
+	}
+	
+
+	public void Append(string playerName, string activity)
+	{
+		lines.Add("<i>"+playerName+"</i> > " + activity);
+
+		int index = Mathf.Max (lines.Count - 10, 0);
+		int count = Mathf.Min (lines.Count, 10);
+
+		ContentChangedEvent.Invoke (String.Join("\n", lines.GetRange(index, count).ToArray ()));
+	}
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+	protected ActivityLog activityLog = null;
+
+	public int linesCount;
+
+	public UnityEvent GameStartedEvent;
+	public UnityEvent GamePausedEvent;
+	public UnityEvent GameResumedEvent;
+	public GameOverEventClass GameOverEvent; [Serializable] public class GameOverEventClass : UnityEvent<bool> {}
+	public LinesCountChangedEventClass LinesCountChangedEvent; [Serializable] public class LinesCountChangedEventClass : UnityEvent<int> {}
+
+
+	void Awake()
+	{
+		Services.Register<GameManager> (this);
+	}
+
+
+	void Start ()
+	{
+		StartGame ();
+	}
+
+
+	public void ResetGame()
+	{
+		linesCount = 0;
+	}
+
+
+	public void StartGame()
+	{
+		activityLog = Services.Find<ActivityLog> ();
+
+		ResetGame ();
+
+		activityLog.Append("System", "Game started");
+		GameStartedEvent.Invoke();
+	}
+
+
+	public void OnFullLineFound(int y)
+	{
+		linesCount++;
+		LinesCountChangedEvent.Invoke (linesCount);
+	}
+
+
+}
+
